0317fb10728783abc4d0e4f3098a5bcd968d3db5
Zdzislaw Trzaska <ztrzaska2@gmail.com>
2019-06-23 14:05:45 +0200

    Logowanie

--- src/main/java/pl/sda/config/SecurityConfig.java
+++ src/main/java/pl/sda/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .usernameParameter("username")
                 .passwordParameter("pass")
                 .defaultSuccessUrl("/index", true)
-                .failureUrl("/error");
+                ;
         // .failureHandler(authenticationFailureHandler())
 
         http.csrf().disable()
