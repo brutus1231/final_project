0127a095b82babc12a4c70d315defb2fbbeb7c1e
Zdzislaw Trzaska <ztrzaska2@gmail.com>
2019-06-23 13:50:41 +0200

    Initial commit

---
+++ src/main/java/pl/sda/config/FinalProjectApplication.java
@@ -0,0 +1,17 @@
+package pl.sda.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+@ComponentScan("pl.sda")
+@Import(SecurityConfig.class)
+public class FinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/pl/sda/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package pl.sda.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("{noop}user").roles("USER");
+        auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("dba").password("{noop}dba").roles("DBA");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                .antMatchers("/dba/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_DBA')")
+                .antMatchers("/login*").permitAll()
+                .antMatchers("/register*").permitAll()
+                .anyRequest().authenticated();
+
+        http.formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/appLogin")
+                .usernameParameter("username")
+                .passwordParameter("pass")
+                .defaultSuccessUrl("/index", true)
+                .failureUrl("/error");
+        // .failureHandler(authenticationFailureHandler())
+
+        http.csrf().disable()
+                .headers().frameOptions().disable();
+
+        http.logout()
+                .logoutUrl("/logout")
+                .deleteCookies("JSESSIONID")
+        //  .logoutSuccessHandler(logoutSuccessHandler())
+        ;
+
+    }
+}
